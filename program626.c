5aa81cd69afb5ed59b963421a7fc82c4064b6ef0
Anuraj Ghawate <ghawateanuraj@gmail.com>
2026-08-06 01:07:26 +0530

    Accepting Return Of printf Function
    Accepting Return Of printf Function, It Returns How Many Bytes Data Is Printed(Written) On Standerd Output Device

---
+++ program626.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int iRet = 0;
+   
+    iRet = printf("Jay Ganesh...\n");
+
+    printf("Value Of iRet Is : %d \n",iRet);
+    return 0;
+}
---
+++ program627.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int iRet = 0;
+   
+    iRet = printf("Marvellous\n");
+
+    printf("Value Of iRet Is : %d \n",iRet);
+    return 0;
+}
